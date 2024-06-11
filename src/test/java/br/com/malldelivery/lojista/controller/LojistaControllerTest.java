8d85d93a7f906e8ecca8b560bb965f319380d4ff
rafael.cruz <rafaelcruz.net81@gmail.com>
2024-06-11 12:54:38 -0300

    Ajuste do testes  com autenticação

--- src/test/java/br/com/malldelivery/lojista/controller/LojistaControllerTest.java
+++ src/test/java/br/com/malldelivery/lojista/controller/LojistaControllerTest.java
@@ -5,8 +5,14 @@ import br.com.malldelivery.lojista.model.Endereco;
 import br.com.malldelivery.lojista.model.Loja;
 import br.com.malldelivery.lojista.model.TipoConta;
 import br.com.malldelivery.lojista.request.LojistaRequest;
+import br.com.malldelivery.lojista.seguranca.authentication.JwtTokenService;
+import br.com.malldelivery.lojista.seguranca.authentication.UserAuthenticationFilter;
 import br.com.malldelivery.lojista.service.LojaService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.Filter;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,16 +20,24 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Optional;
 
 import static org.mockito.BDDMockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.testSecurityContext;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 
 @AutoConfigureMockMvc
 @WebMvcTest(controllers = LojistaController.class)
@@ -38,6 +52,12 @@ public class LojistaControllerTest {
     private ObjectMapper objectMapper;
 
     private Loja loja;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @MockBean
+    private UserAuthenticationFilter filter;
 
     @BeforeEach
     public void setup() {
@@ -62,15 +82,23 @@ public class LojistaControllerTest {
         dadoBancario.setCodigoBanco("341");
 
         loja.getDadosBancarios().add(dadoBancario);
+
+        //Configuração de Autenticação para testes
+        this.mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+
     }
 
-
     @Test
+    @WithMockUser(username = "user1", password = "pwd", roles = "USER")
     public void deveConsultarLojistaPorIdComSucesso() throws Exception {
         int id = 1;
         given(this.lojaService.obterLojistaPorId(id)).willReturn(Loja.toResponse(this.loja));
 
-        mvc.perform(MockMvcRequestBuilders.get("/lojista/" + id)
+        mvc.perform(MockMvcRequestBuilders
+                        .get("/lojista/" + id)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(id)))
@@ -78,6 +106,7 @@ public class LojistaControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "user1", password = "pwd", roles = "USER")
     public void deveConsultarLojistaPorIdRetornandoNotFound() throws Exception {
         int id = 1;
         given(this.lojaService.obterLojistaPorId(id)).willReturn(null);
